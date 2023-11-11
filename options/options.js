0caa2a790b6f9836d026e32244b29f91db9849e6
Crowdbotics <team@crowdbotics.com>
2023-11-11 08:03:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ibrahim-tv-44484",
+  url: "https://ibrahim-tv-44484.botics.co",
+  api: "https://ibrahim-tv-44484.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
